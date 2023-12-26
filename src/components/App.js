afd4dbe7e17cd9412536a20e4cfd5f6791fe6d47
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-12-26 12:52:50 +0000

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [tasksList, setTasksList] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTasksList([...tasksList, task]);
+      setTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = tasksList.filter((_, i) => i !== index);
+    setTasksList(updatedTasks);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <header>
+        <h1>To-Do List</h1>
+        <div>
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Enter a new task"
+          />
+          <button onClick={addTask}>Add Todo</button>
+        </div>
+      </header>
 
-export default App
+      <main>
+        <ul>
+          {tasksList.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => removeTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default App;
